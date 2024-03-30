b314ec57202e3d48b5f66e18baf441a877a82fc9
Yash Panchal <88079566+Yashpanchal2210@users.noreply.github.com>
2024-03-30 13:14:33 +0530

    First Comit

---
+++ UrbanAutoWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UrbanAutoWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
