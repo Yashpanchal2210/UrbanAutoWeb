2991c3289c36a411effb16d913c56edd27aedce2
Yash Panchal <88079566+Yashpanchal2210@users.noreply.github.com>
2024-04-14 21:47:23 +0530

    changes

--- UrbanAutoWeb/obj/Debug/net8.0/UrbanAutoWeb.AssemblyInfo.cs
+++ UrbanAutoWeb/obj/Debug/net8.0/UrbanAutoWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UrbanAutoWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be078166a7fe47fb3eab3150e54d71a83364178b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25fef3be24d8501a32f695fb45df0f44df852e5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("UrbanAutoWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UrbanAutoWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
